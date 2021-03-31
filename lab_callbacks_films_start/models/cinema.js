c34001d1974dde9a480147bc11cc7c2adb8f790f
jaeltg <jaeltg@hotmail.com>
2021-03-31 16:04:01 +0100

    added MVP

--- lab_callbacks_films_start/models/cinema.js
+++ lab_callbacks_films_start/models/cinema.js
@@ -2,4 +2,54 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.getFilmTitles = function() {
+  const filmTitleArray = this.films.map((film) => {
+    return film.title
+  })
+  return filmTitleArray
+};
+
+Cinema.prototype.findFilmByTitle = function(title) {
+  const foundFilm = this.films.filter((film) => {
+    return film.title === title
+  })
+  return foundFilm[0]
+}
+//   let foundFilm = null;
+//   this.films.forEach((film) => {
+//     if (film.title === title) {
+//       foundFilm = film
+//     }
+//   })
+//   return foundFilm
+
+
+Cinema.prototype.filterFilmsByGenre = function(genre) {
+  return this.films.filter((film) => {
+    return film.genre === genre
+  })
+}
+
+Cinema.prototype.checkIfFilmsFromYear = function(year) {
+  return this.films.some((film) => {
+    return film.year === year
+  })
+}
+
+Cinema.prototype.checkIfMinimumLength = function(length) {
+  return this.films.every((film) => {
+    return film.length >= length
+  })
+}
+
+Cinema.prototype.totalRunningTime = function() {
+  return this.films.reduce((totalFilmLength, film) => {
+    return totalFilmLength + film.length
+  }, 0)
+}
+
+
+ 
+
+
 module.exports = Cinema;
